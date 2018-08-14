6d3f2fe492ccdb70b4f37f165c134b347dc740a3
Nicklas Moberg <nicklas.moberg@telia.com>
2018-08-14 15:39:15 +0200

    Fix `compose`

--- webpack/js/fun/compose.js
+++ webpack/js/fun/compose.js
@@ -1,1 +1,1 @@
-export const compose = (...fns) => (x) => fns.reduce((acc, fn) => fn(acc), x)
+export const compose = (...fns) => (x) => fns.reduceRight((acc, fn) => fn(acc), x)
--- webpack/js/fun/compose.spec.js
+++ webpack/js/fun/compose.spec.js
@@ -13,14 +13,16 @@ describe(`compose`, () => {
 
   it(`should map functions over value`, () => {
     const expected = 5
-    const actual = compose(multiplyTwo, addOne)(2)
+    const actual = compose(addOne, multiplyTwo)(2)
 
     expect(actual).to.equal(expected)
   })
 
   it(`should map all functions over value`, () => {
     const expected = 6
-    const actual = compose((x) => x.a, addOne, multiplyTwo)({ a: 2 })
+    const actual = compose(multiplyTwo, addOne, (x) => x.a)({
+      a: 2
+    })
 
     expect(actual).to.equal(expected)
   })
